a60fc370265748c105da640e97f40e7b441b0dff
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2016-11-14 19:52:57 -0700

    Updated Multi-Player

--- app/game.js
+++ app/game.js
@@ -7,85 +7,66 @@ angular.module('con4', [])
 function GameController() {
 
 		var gc = this
+		var victory = false
+		var count = 0
+		var currentPlayer = 0
+		var activeSelectedPlayers = 2
+		var gridHeight
+		var gridWidth
+		var totalPlayers = ['Red', 'Black', 'Yellow', 'Green', 'Orange']
+		gc.selectedPlayers
+		gc.grid = []
+		gc.activePlayer = 'Red'
+
+		
 
 		gc.newGame = function () {
-		/**
-		 * set victory to false
-		 * gc.grid = buildGrid();
-		 * This is connect 4 so red plays first
-		 */
+			victory = false
+			count = 0
+			currentPlayer = 0
+			gc.activePlayer = 'Red'
+			activeSelectedPlayers = gc.selectedPlayers
+			gridHeight = (parseInt(activeSelectedPlayers)-2) + 6
+			gridWidth= (parseInt(activeSelectedPlayers)-2) + 8
+		 	gc.grid = buildGrid(gridHeight, gridWidth);
 		}
 
-		function buildGrid() {
-		//Build a 6x7 grid object and return it from this function
-		//Each cell of the grid is an object that knows its coords
-		/**
-		 * Cell Schema
-		 * {
-		 * 		row: number,
-		 * 		col: number
-		 * }
-		 */
-
-		//Once you finishe building your grid make sure gc.newGame is setting 
-		//gc.grid = buildGrid();
-		//If your build grid is working correctly you can start up your server to see the grid
-		//drawn to the screen.
+		function buildGrid(vert, horz) {
+			var grid = [];
+			for (var x = 0; x < vert; x++) {
+				grid.push([]);
+			for (var y = 0; y < horz; y++) {
+				grid[x].push({row:x, col:y}); 
+				}
+			}
+			return grid
 		}
 
 		gc.dropToken = function (col) {
-		//The col is passed in from the view
-		//Column is full no space available
-		//Bad Drop
-		if (gc.grid[0][col].hasToken) {
-			return;
-		}
-
-		//Find the southMost unoccupied row
-		/**
-		 * Always start at row 0 and then increment
-		 * until you have reached the final row or 
-		 * found a cell that already has a token
-		 */
+		//if Column is full no space available cancels drop
+		if (gc.grid[0][col].hasToken) { return }
 		var row = checkSouth(0, col);
+		var cell = gc.grid[row][col]
+		cell.hasToken = true
 
-		/**
-		 * Once the row is identified
-		 * set the cell by accessing 
-		 * gc.grid[row][col]
-		 * set cell.hasToken = true
-		 * set cell.color gc.activePlayer
-		 **/
-
-		//endTurn and checkVictory
+		cell.color = gc.activePlayer
+		checkVictory(cell)
+		checkCount()
+		endTurn()
 		}
 
 		function checkSouth(row, col) {
-		/**
-		 * Let's use recursion
-		 * A recursive function is...
-		 * a function that calls itself
-		 * until some condition is met
-		 * 
-		 * Check South will need essentially two base cases
-		 * 
-		 */
-
-		//Base case 1 found south Token return row - 1 to go back one step
-
-		//base case 2 reached bottom of grid return row or 5
-
-		/**
-		 * if neither base case 
-		 * (***increment row***, then return checkSouth())
-		 * make sure to pass the arguments through
-		 */
+		if(row> gridHeight-1){ return gridHeight - 1}
+		if(gc.grid[row][col].hasToken){
+			row --
+			return row
+		}else{
+			row ++
+			return checkSouth(row, col)
+		}
 		}
 
 		function checkVictory(cell) {
-		//This one is a gimme you shouldn't have to change anything here
-		//Once you fix the checkNextCell function the green squiggles should dissapear.
-		//If they don't make sure you are returning a number from the checkNextCell function
 
 		var horizontalMatches = 0;
 		//Check Horizontal
@@ -105,50 +86,80 @@ function GameController() {
 		var diagRight = 0;
 		diagRight += checkNextCell(cell, 0, 'diagUpRight');
 		diagRight += checkNextCell(cell, 0, 'diagBotLeft');
-
 		if (verticalMatches >= 3 || horizontalMatches >= 3 || diagLeft >= 3 || diagRight >= 3) {
-			//You can do better than an alert 
 			alert(cell.color + ' Wins');
 		}
 		}
 
 		function getNextCell(cell, direction) {
-		/**
-		 * var nextRow = cell.row;
-		 * var nextCol = cell.col;
-		 * 
-		 * adjust the values of nextRow
-		 * and nextCol as needed based upon
-		 * the direction of travel.
-		 * 
-		 * if nextRow > 0 or < 5 
-		 * or if nextCol > 6 
-		 * return null;
-		 * 
-		 * otherwise 
-		 * return gc.grid[nextRow][nextCol];
-		 */
+		  var nextRow = cell.row;
+		  var nextCol = cell.col;
+		  
+		  //adjusts the values of nextRow
+		  //and nextCol as needed based upon
+		  //the direction of travel.
+		  switch(direction){
+			case 'left':
+				nextCol--
+				break;
+			case 'right':
+				nextCol++
+				break;
+			case 'bottom':
+				nextRow++
+				break;
+			case 'diagUpLeft':
+				nextRow--
+				nextCol--
+				break;
+			case 'diagBotRight':
+				nextRow++
+				nextCol++
+				break;
+			case 'diagUpRight':
+				nextRow--
+				nextCol++
+				break;
+			case 'diagBotLeft':
+				nextRow++
+				nextCol--
+				break;
+		  }
+		  if(nextRow < 0 || nextRow > gridHeight-1){return null}
+		  if(nextCol > gridWidth) {return null}
+
+		  return gc.grid[nextRow][nextCol];
+		 
 		}
 
 		function checkNextCell(cell, matches, direction) {
-		/**
-		 * var nextCell = getNextCell(cell, direction)
-		 * check if nextCell is defined 
-		 * if nextCell.hasToken and nextCell.color
-		 * matches cell.color 
-		 * increment matches and then 
-		 * return checkNextCell(nextCell, matches, direction)
-		 * 
-		 * otherwise return matches
-		 */
+			var nextCell = getNextCell(cell, direction)
+			var matches = matches
+			if(nextCell){
+				if(nextCell.hasToken && cell.color == nextCell.color){
+					matches++
+					return checkNextCell(nextCell, matches, direction)
+				}
+			}
+			return matches
 		}
 
 		function endTurn() {
-		/**
-		 * End Turn simply switch 
-		 * gc.activePlayer from 
-		 * 'red' to 'yellow' 
-		 * and 'yellow' to 'red'
-		 */
+			tempPlayer = parseInt(activeSelectedPlayers)
+			if(currentPlayer >=  tempPlayer- 1){
+				currentPlayer = 0
+			}else{
+				currentPlayer++
+			}
+			gc.activePlayer = totalPlayers[currentPlayer]
+		}
+
+		
+	 	function checkCount(){
+			if(count == ((gridHeight*gridWidth)-1)){
+				alert('Tie Game!')
+			}else{
+				count++
+			}
 		}
 };
